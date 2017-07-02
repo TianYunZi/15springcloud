5600629a337711f360ddc0810986673fe6da1ebc
zxysegh@gmail.com <20150130TUESDAy>
2017-07-02 22:03:02 +0800

    spring cloud config(未成功)

--- 8.1.1.spring.cloud.config/src/main/java/com/cloud/Ch811Application.java
+++ 8.1.1.spring.cloud.config/src/main/java/com/cloud/Ch811Application.java
@@ -2,12 +2,14 @@ package com.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by Admin on 2017/6/30.
  */
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Ch811Application {
 
